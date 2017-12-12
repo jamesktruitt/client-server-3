40733d525fa244de8dcb467bfa607bf8460ce3cc
James Truitt <tru@rockyagirl.com>
2017-12-12 10:48:08 -0700

    version-3 complete

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./data/students")
+const app = express();
+app.use(cors());
+
+function getById(data, id) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
+    }
+  }
+  return null;
+};
+
+app.get("/", (request, response) => {
+  response.json(data);
+});
+
+app.get("/:id", (request, response) => {
+  const record = getById(data, request.params.id);
+  if (!record) {
+    response.status(404).json({
+      error: {
+        message: "No record found!"
+      }
+    });
+  } else {
+    response.json(record);
+  }
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ data/students.js
@@ -0,0 +1,21 @@
+module.exports = [{
+  "id": 1,
+  "fullName": "Alice Zephyr",
+  "homeTown": "Seattle"
+},{
+  "id": 2,
+  "fullName": "Bob Yellow",
+  "homeTown": "Vancouver"
+},{
+  "id": 3,
+  "fullName": "Claire Xylitol",
+  "homeTown": "Toledo"
+},{
+  "id": 4,
+  "fullName": "Daniel Winston",
+  "homeTown": "Akron"
+},{
+  "id": 5,
+  "fullName": "Edina Veritas",
+  "homeTown": "Wichita"
+}];
